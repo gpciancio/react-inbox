84d4cfd7548833962dc86514460061afab0db1c2
gpciancio <gopalciancio@gmail.com>
2017-07-17 15:23:17 -0600

    toolbar loading

---
+++ src/MessageList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// const displayMessages = function (){
+//   return().map(messages);
+// }
+//
+// displayMessages();
+// export default class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             id: 0;
+//             subject: "";
+//             read: true;
+//             starred: true;
+//             labels: [];
+//         };
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <CreateTodo todos={this.state.id} createTask={this.createTask.bind(this)} />
+//                 <TodosList
+//                     todos={this.state.todos}
+//                     toggleTask={this.toggleTask.bind(this)}
+//                     saveTask={this.saveTask.bind(this)}
+//                     deleteTask={this.deleteTask.bind(this)}
+//                 />
+//             </div>
+//         );
+//     }
